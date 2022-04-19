c3ebd7baa83c55b2f52762713d59f0fd90baec80
Desktop <charlesworld1994@gmail.com>
2022-04-18 18:29:20 -0700

    initial blog commit

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Hello from Index</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
